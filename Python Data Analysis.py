272d56976f5847c5b5add2b3f4d1b95a6a5bc2a1
Vlad Lee <vlad7984@gmail.com>
2024-12-11 01:13:50 -0500

    Python Data Analysis.py

--- Python Data Analysis.py
+++ Python Data Analysis.py
@@ -65,8 +65,36 @@ plt.show()
 #Merge order_items, products on product_id
 df_order_items = pd.merge(df_order_items, df_products[['product_id', 'product_category_name_english']], on='product_id', how='left')
 
+#Merge orders, order_items, on order_id
+df_orders = pd.merge(df_orders, df_order_items[['order_id', 'price']], on='order_id', how='left')
+
 #merge orders and customers on order_id
-df_orders = pd.merge(df_orders, df_customers[['state_name']], on = 'customer_id', how='left')
+df_orders = pd.merge(df_orders, df_customers[['customer_id','state_name']], on = 'customer_id', how='left')
+
+#Create order year column
+df_orders['order_purchase_year'] = pd.to_datetime(df_orders['order_purchase_timestamp']).dt.year
+
+
+#Group purchases by purchase_year and state
+state_price_sum = df_orders.groupby(['state_name', 'order_purchase_year'])['price'].sum().reset_index().sort_values(by='price', ascending=False)
+
+#Group number of orders by purchase_year and state
+orders_by_state_year = df_orders.groupby(['state_name', 'order_purchase_year']).size().reset_index(name='order_count')
+
+
+#Average Delivery Time Analysis
+
+#Create order year column
+df_orders['order_year'] = pd.to_datetime(df_orders['order_approved_at']).dt.year
+df_orders['deliver_year'] = pd.to_datetime(df_orders['order_delivered_customer_date']).dt.year
+
+#Create new columns for delivery time and estimate delta (the difference between estimated delivery date and actual delivery date)
+df_orders['deliver_time'] = (pd.to_datetime(df_orders['order_delivered_customer_date']) - pd.to_datetime(df_orders['order_approved_at'])).dt.days
+df_orders['estimate_delta'] = (pd.to_datetime(df_orders['order_estimated_delivery_date']) - pd.to_datetime(df_orders['order_delivered_customer_date'])).dt.days
+
+#Calculate average delivery by state and by state/year
+delivery_by_state = df_orders.groupby(['state_name'])['deliver_time'].mean()
+delivery_by_state_year = df_orders.groupby(['state_name', 'deliver_year'])['deliver_time'].mean().unstack(fill_value=0)
 
 
 
