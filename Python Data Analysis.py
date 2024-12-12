c85b8476a987e9528985389427d1525705f7a8a7
Vlad Lee <vlad7984@gmail.com>
2024-12-12 00:03:10 -0500

    Python Data Analysis.py

--- Python Data Analysis.py
+++ Python Data Analysis.py
@@ -9,6 +9,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 plt.style.use('ggplot')
 
 os.chdir('C:/Users/vlad7/NYCDSA-Python-Project/Raw Data')
@@ -33,8 +34,41 @@ df_sellers.rename(columns={'seller_state': 'state'}, inplace=True)
 df_customers = pd.merge(df_customers, df_states, on = 'state')
 df_sellers = pd.merge(df_sellers, df_states, on = 'state')
 
-#Merge products product category translations to obtain english categories.
-df_products = pd.merge(df_products, df_product_translations, on = 'product_category_name')
+#Merge products and product category translations to obtain english categories.
+df_products = pd.merge(df_products, df_product_translations, on = 'product_category_name', how='left')
+
+#Calculate total price/average review for each order as there are duplicate order IDs
+df_order_items_price_agg = df_order_items.groupby('order_id').agg({'price': 'sum'}).reset_index()
+df_order_reviews_avg = df_order_reviews.groupby('order_id').agg({'review_score': 'mean'}).reset_index()
+
+#Merge order items and reviews
+df_order_items = pd.merge(df_order_items, df_order_reviews_avg, on = 'order_id', how = 'left')
+
+#Merge aggregate price, product name, and state data into the Orders dataframe.
+df_order_items = pd.merge(df_order_items, df_products[['product_id', 'product_category_name_english']], on='product_id', how='left')
+df_orders = pd.merge(df_orders, df_order_items_price_agg[['order_id', 'price']], on='order_id', how='left')
+df_orders = pd.merge(df_orders, df_customers[['customer_id','state_name']], on = 'customer_id', how='left')
+df_orders = pd.merge(df_orders, df_order_reviews_avg[['order_id','review_score']], on = 'order_id', how='left')
+
+#Create order/deliver year, delivery time, and diff between estimate and actual delivery columns
+df_orders['order_purchase_year'] = pd.to_datetime(df_orders['order_purchase_timestamp']).dt.year
+df_orders['deliver_year'] = pd.to_datetime(df_orders['order_delivered_customer_date']).dt.year
+df_orders['deliver_time'] = (pd.to_datetime(df_orders['order_delivered_customer_date']) - pd.to_datetime(df_orders['order_approved_at'])).dt.days
+df_orders['estimate_delta'] = (pd.to_datetime(df_orders['order_delivered_customer_date']) - pd.to_datetime(df_orders['order_estimated_delivery_date'])).dt.days
+
+#Group purchases
+state_price_sum = df_orders.groupby(['state_name'])['price'].sum().reset_index().sort_values(by='price', ascending=False)
+orders_by_state_year = df_orders.groupby(['state_name', 'order_purchase_year']).size().reset_index(name='order_count')
+
+#Calculate average delivery by state and by state/year
+delivery_by_state = df_orders.groupby(['state_name'])['deliver_time'].mean().reset_index()
+delivery_by_state_year = df_orders.groupby(['state_name', 'deliver_year'])['deliver_time'].mean().unstack(fill_value=0).reset_index()
+
+#Calculate number of orders and reviews
+num_reviews = df_orders['review_score'].count()
+num_orders = df_orders.shape[0]
+percent_orders_with_reviews = num_reviews / num_orders
+print(f'Percent of orders with reviews: {percent_orders_with_reviews:.2%}')
 
 #Find distribution of customers by state for top 10 states.
 customer_state_counts = df_customers['state_name'].value_counts()
@@ -52,50 +86,108 @@ plt.xlabel('State')
 plt.ylabel('Seller Count')
 plt.show()
 
-#Filter out 'not_defined' payment types.
-df_order_payments['payment_type'] = df_order_payments['payment_type'].str.strip().str.lower()
+#Find distribution of reviews
+reviews = df_order_reviews['review_score'].value_counts()
+
+ax = reviews.head(10).sort_values(ascending=True).plot(kind='bar', color='skyblue')
+plt.title('Number of Reviews')
+plt.xlabel('Review Score')
+plt.ylabel('Number of Reviews')
+plt.xticks(rotation=0)
+plt.grid(False)
+plt.gcf().set_facecolor('white')
+ax.set_facecolor('white')
+plt.show()
+
+reviews.plot(kind='pie', figsize = (10,10), fontsize = 10, autopct='%1.1f%%')
+plt.title('Distribution of Reviews')
+plt.xlabel('')
+plt.ylabel('')
+plt.show()
+
+#Filter out 'not_defined' payment types. Strip leading and trailing white space, replace underscore with blank space
+df_order_payments['payment_type'] = df_order_payments['payment_type'].str.strip().str.title()
+df_order_payments['payment_type'] = df_order_payments['payment_type'].str.replace('_', ' ')
 df_order_payments = df_order_payments[df_order_payments['payment_type'] != 'not_defined']
+
+df_order_items['product_category_name_english'] = df_order_items['product_category_name_english'].str.strip().str.title()
+df_order_items['product_category_name_english'] = df_order_items['product_category_name_english'].str.replace('_', ' ')
+
 #Find proportion of payment types
-df_order_payments['payment_type'].value_counts().plot(kind='pie', figsize = (10,10), fontsize = 10, autopct='%1.1f%%')
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'pink']
+df_order_payments['payment_type'].value_counts().plot(kind='pie', colors=colors, figsize = (10,10), fontsize = 10, autopct='%1.1f%%')
 plt.title('Proportion of Payment Types')
 plt.ylabel('')
 plt.xlabel('')
 plt.show()
 
-#Merge order_items, products on product_id
-df_order_items = pd.merge(df_order_items, df_products[['product_id', 'product_category_name_english']], on='product_id', how='left')
+#Box plot of delivery times for each state
+sns.boxplot(x='state_name', y='deliver_time', data=df_orders)
+plt.title('Delivery Time by State')
+plt.xlabel('State')
+plt.ylabel('Delivery Time (Days)')
+plt.xticks(rotation=90)
+plt.grid(False)
+plt.show()
 
-#Merge orders, order_items, on order_id
-df_orders = pd.merge(df_orders, df_order_items[['order_id', 'price']], on='order_id', how='left')
+#Box plot of differnce between delivery estimate and actual delivery times for each state
+sns.boxplot(x='state_name', y='estimate_delta', data=df_orders)
+plt.title('Delivery Estimate Delta by State')
+plt.xlabel('State')
+plt.ylabel('Actual Delivery Minus Estimate (Days)')
+plt.xticks(rotation=90)
+plt.grid(False)
+plt.show()
 
-#merge orders and customers on order_id
-df_orders = pd.merge(df_orders, df_customers[['customer_id','state_name']], on = 'customer_id', how='left')
+#Calculate correlation between delivery delta and review score.
+correlation = df_orders['estimate_delta'].corr(df_orders['review_score'])
+corr_text = f'Pearson R: {correlation:.2f}'
 
-#Create order year column
-df_orders['order_purchase_year'] = pd.to_datetime(df_orders['order_purchase_timestamp']).dt.year
+sns.regplot(x='estimate_delta', y='review_score', data=df_orders, line_kws={'color': 'teal'},scatter_kws={'s': 10, 'alpha': 0.5})
+plt.xlabel('Actual Delivery Minus Estimate (Days)')
+plt.ylabel('Review Score')
+plt.ylim(0, 10)
+plt.xlim(-200,200)
+plt.title('Review Score vs Actual Delivery Minus Estimate')
+plt.grid(False)
+plt.text(x=0.05, y=9.5, s=corr_text, fontsize=12, color='teal')
+plt.show()
 
+#Calculate correlation between delivery time and review score.
+correlation2 = df_orders['deliver_time'].corr(df_orders['review_score'])
+corr_text2 = f'Pearson R: {correlation2:.2f}'
 
-#Group purchases by purchase_year and state
-state_price_sum = df_orders.groupby(['state_name', 'order_purchase_year'])['price'].sum().reset_index().sort_values(by='price', ascending=False)
+sns.regplot(x='deliver_time', y='review_score', data=df_orders, line_kws={'color': 'green'},scatter_kws={'s': 10, 'alpha': 0.5})
+plt.xlabel('Delivery Time')
+plt.ylabel('Review Score')
+plt.ylim(0, 10)
+plt.title('Review Score vs. Delivery Time')
+plt.grid(False)
+plt.text(x=0.05, y=9.5, s=corr_text2, fontsize=12, color='teal')
+plt.show()
 
-#Group number of orders by purchase_year and state
-orders_by_state_year = df_orders.groupby(['state_name', 'order_purchase_year']).size().reset_index(name='order_count')
+#Calculate correlation between price and review score.
+correlation3 = df_orders['price'].corr(df_orders['review_score'])
+corr_text3 = f'Pearson R: {correlation3:.2f}'
 
+sns.regplot(x='price', y='review_score', data=df_orders, line_kws={'color': 'purple'},scatter_kws={'s': 10, 'alpha': 0.5})
+plt.xlabel('Price')
+plt.ylabel('Review Score')
+plt.ylim(0, 10)
+plt.title('Review Score vs. Price Time')
+plt.grid(False)
+plt.text(x=0.05, y=9.5, s=corr_text3, fontsize=12, color='teal')
+plt.show()
 
-#Average Delivery Time Analysis
+#Group order items by category and count number of reviews
+order_count_by_category = df_order_items.groupby('product_category_name_english')['order_id'].nunique()
 
-#Create order year column
-df_orders['order_year'] = pd.to_datetime(df_orders['order_approved_at']).dt.year
-df_orders['deliver_year'] = pd.to_datetime(df_orders['order_delivered_customer_date']).dt.year
-
-#Create new columns for delivery time and estimate delta (the difference between estimated delivery date and actual delivery date)
-df_orders['deliver_time'] = (pd.to_datetime(df_orders['order_delivered_customer_date']) - pd.to_datetime(df_orders['order_approved_at'])).dt.days
-df_orders['estimate_delta'] = (pd.to_datetime(df_orders['order_estimated_delivery_date']) - pd.to_datetime(df_orders['order_delivered_customer_date'])).dt.days
-
-#Calculate average delivery by state and by state/year
-delivery_by_state = df_orders.groupby(['state_name'])['deliver_time'].mean()
-delivery_by_state_year = df_orders.groupby(['state_name', 'deliver_year'])['deliver_time'].mean().unstack(fill_value=0)
-
-
-
-
+order_count_by_category.head(10).sort_values(ascending=True).plot(kind='bar', color='orange')
+plt.title('Number of Reviews by Category')
+plt.xlabel('Category')
+plt.ylabel('Number of Reviews')
+plt.xticks(rotation=0)
+plt.grid(False)
+plt.xticks(rotation=90)
+plt.gcf().set_facecolor('white')
+plt.show()
